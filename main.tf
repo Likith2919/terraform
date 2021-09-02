0a51f90c813726e8eecb66d8f72fed7c9f21c7c0
Likith2919 <72124258+Likith2919@users.noreply.github.com>
2021-09-02 12:56:50 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,4 @@
-provider "aws" {
-    access_key = "AKIA3ZHUTQOV6JVLUGUS"
-    secret_key = "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"
-    region = "us-west-2"
-  
-}
+
 resource "aws_vpc" "vpc" {
         cidr_block = "10.0.0.0/16"
         enable_dns_hostnames = true
